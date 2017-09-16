e40ad55d44b965da6c76a4de947ff7db8f77d59d
Franccesco Orozco <francc3sco@gmail.com>
2017-09-16 00:20:20 -0600

    overwrite, append and backup functions.

--- pia_patcher.py
+++ pia_patcher.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+# Author: Franccesco Orozco
+# Version: 0.1
 
 "Replace resolv.conf with Private Internet Access DNS's"
 
 import shutil
 import os
+from sys import exit
 
 
 def check_current_dir():
@@ -32,10 +35,22 @@ def backup_resolv():
 def append_to_resolv():
     """Append template to resolv.conf instead of replacing it."""
     current_dir = check_current_dir()
-    with open(current_dir + 'pia_dns_template.txt', mode='r') as template_obj:
-        pia_template = template_obj.read()
-    with open('/etc/resolv.conf', mode='a') as resolv_obj:
-        resolv_obj.write(pia_template)
+    try:
+        with open(current_dir + 'pia_dns_template.txt', mode='r') as temp_obj:
+            pia_template = temp_obj.read()
+        with open('/etc/resolv.conf', mode='a') as resolv_obj:
+            # double return so it doesn't get cluttered
+            resolv_obj.writelines("\n\n")
+            resolv_obj.write(pia_template)
+    except PermissionError as denied:
+        denied_message = denied
+        print(denied_message)
+        print("\033[1m" + "Waddup, run me as ROOT okay?." + "\033[0;0m")
+        print("Breaking...")
+        exit()
+    else:
+        print("DNS successfully patched (Appended).")
+        return True
 
 
 def replace_resolv():
@@ -49,8 +64,36 @@ def replace_resolv():
         print("\033[1m" + "Whoops! Run it as root buddy." + "\033[0;0m")
     else:
         print("DNS successfully patched.")
+        return True
 
 
 if __name__ == '__main__':
     """Run if script called directly."""
-    if 
+    # Case scenario: if resolv exists, overwrite, append or break.
+    #                but make a backup first!
+    if check_resolv_exist():
+        print("Warning: resolv.conf exist.")
+        print("[O]verwrite / [A]ppend / [B]reak")
+        override_safety = input("\033[1m" + "~> " + "\033[0;0m")
+
+        if override_safety.lower() == 'o':
+            print("\nMaking a backup first...")
+            backup_resolv()
+
+            print("Replacing file...")
+            replace_resolv()
+
+        elif override_safety.lower() == 'a':
+            print("\nMaking a backup first.")
+            backup_resolv()
+
+            print("Appending to resolv.conf")
+            append_to_resolv()
+
+        elif override_safety.lower() == 'b':
+            print('See ya.')
+            exit()
+
+        else:
+            print("Wrong option I guess? Exiting...")
+            exit()
